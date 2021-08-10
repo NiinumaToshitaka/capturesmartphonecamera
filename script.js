0e4274cd950c99069c7ef6bffd3dbf74795a397b
Niinuma Toshitaka <niinuma.toshitaka@gmail.com>
2021-08-10 12:34:25 +0900

    コードのフォーマット調整

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
  * 動画のキャプチャを開始する
  */
 function start_video_stream() {
-    var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
+    // 適当にvideoタグのオブジェクトを取得
+    var video = document.getElementById("myVideo");
     // 映像・音声を取得するかの設定
-    // 映像はバックカメラ（環境側カメラ）を使用する
-    var constrains = { video: { facingMode: "environment" }, audio: false };
+    const constrains = {
+        // 映像はバックカメラ（環境側カメラ）を使用する
+        video: {
+            facingMode: "environment"
+        },
+        // 音声は不要なので使用しない
+        audio: false
+    };
     navigator.mediaDevices
         .getUserMedia(constrains)
         .then(function (stream) {
-            video.srcObject = stream; // streamはユーザーのカメラとマイクの情報で、これをvideoの入力ソースにする
+            // streamはユーザーのカメラとマイクの情報で、これをvideoの入力ソースにする
+            video.srcObject = stream;
             video.play();
         })
         .catch(function (err) {
