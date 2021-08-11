2fbc1693867d76ca9e1b55bf9c17a619075a5455
Niinuma Toshitaka <niinuma.toshitaka@gmail.com>
2021-08-11 15:51:45 +0900

    カメラから画像を取得してcanvasに描画する

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+/** 適当にvideoタグのオブジェクトを取得 */
+const video = document.getElementById("myVideo");
+/** キャンバスオブジェクト */
+const canvas = document.getElementById("picture");
+
 /**
  * 動画のキャプチャを開始する
  */
 function start_video_stream() {
-    // 適当にvideoタグのオブジェクトを取得
-    var video = document.getElementById("myVideo");
     // 映像・音声を取得するかの設定
     const constrains = {
         // 映像はバックカメラ（環境側カメラ）を使用する
@@ -21,9 +24,21 @@ function start_video_stream() {
             video.play();
         })
         .catch(function (err) {
-            console.log("An error occured! " + err);
+            console.log(err.name + ": " + err.message);
+        });
+}
+
+/** カメラから画像を取得してcanvasを更新する */
+function update_canvas() {
+    document.getElementById("shutter")
+        .addEventListener("click", () => {
+            const ctx = canvas.getContext("2d");
+            // canvasに画像を貼り付ける
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         });
 }
 
 // 動画のキャプチャを開始
 start_video_stream();
+// カメラから画像を取得してcanvasを更新する
+update_canvas();
