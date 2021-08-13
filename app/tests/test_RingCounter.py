11dd022dcac8b76882511536146689eca3cb7a9d
Niinuma Toshitaka <niinuma.toshitaka@gmail.com>
2021-08-13 21:53:45 +0900

    `RingCounter`クラスのテストケースを実装
    テストできるコードはテストしておいたほうがいいので実装。

---
+++ app/tests/test_RingCounter.py
@@ -0,0 +1,36 @@
+"""RingCounterクラスのテストケース"""
+
+import unittest
+import service
+
+
+class TestRingCounter(unittest.TestCase):
+    """RingCounterクラスのテストケース"""
+
+    __COUNT_LIMIT = 10
+    """カウンタの最大値"""
+
+    def test_increment(self):
+        """カウントする"""
+        counter = service.RingCounter(TestRingCounter.__COUNT_LIMIT)
+        """カウンタ"""
+        # カウントする
+        counter.increment()
+        current_count = counter.get_count()
+        """現在のカウンタの値"""
+        EXPECT_COUNT = 1
+        """期待するカウンタの値"""
+        self.assertEqual(EXPECT_COUNT, current_count)
+
+    def test_count_limit(self):
+        """規定回数カウントしたら、カウンタが0に戻る"""
+        counter = service.RingCounter(TestRingCounter.__COUNT_LIMIT)
+        """カウンタ"""
+        # 規定回数カウント
+        for i in range(0, TestRingCounter.__COUNT_LIMIT):
+            counter.increment()
+        current_count = counter.get_count()
+        """現在のカウンタの値"""
+        EXPECT_COUNT = 0
+        """期待するカウンタの値"""
+        self.assertEqual(EXPECT_COUNT, current_count)
