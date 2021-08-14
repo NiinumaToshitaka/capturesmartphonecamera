580293247e5d1f8c8958eb74514d2fd70521f790
Niinuma Toshitaka <niinuma.toshitaka@gmail.com>
2021-08-14 13:43:22 +0900

    `MotionDetection`クラスのテストケースを`tests`ディレクトリに移動
    テストケースが`tests`ディレクトリの外側にあるのはおかしいのでまとめる。

--- app/tests/test_MotionDetection.py
+++ app/tests/test_MotionDetection.py
@@ -1,7 +1,13 @@
 """MotionDetectionクラスによる動体検知のテスト
 
-プロジェクトのルートディレクトリで実行すること。
-`python app/[this_module].py`
+テスト用の画像は、このファイルと同じディレクトリにある
+`images`ディレクトリに格納すること。
+
+実行するには、プロジェクトのルートディレクトリで以下を実行すること。
+```bash
+cd app
+python -m tests.test_MotionDetection
+```
 """
 
 import cv2
@@ -10,7 +16,7 @@ from python_modules import MotionDetection
 
 def test_motion_detection():
     """MotionDetectionクラスによる動体検知のテスト"""
-    SAVE_PATH = "images/img{:05d}.jpg"
+    SAVE_PATH = "tests/images/img{:05d}.jpg"
     """画像の保存先パス"""
     WINDOW_NAME = "motion detection result"
     """検知結果を表示するウィンドウ名"""
@@ -31,10 +37,10 @@ def test_motion_detection():
     cv2.waitKey(0)
 
     # 動体検知
-    status, detected_area = motion_detector.detect(frame_0)
+    detection_result = motion_detector.detect(frame_0)
     # 初回実行時は比較するべき前フレームが存在しないので、
     # 検知結果が必ずFalseになる
-    assert status is False
+    assert detection_result.has_detect is False
 
     # 画像ファイルパスを生成
     filepath_1 = SAVE_PATH.format(1)
@@ -50,12 +56,13 @@ def test_motion_detection():
     cv2.waitKey(0)
 
     # 動体検知
-    status, detected_area = motion_detector.detect(frame_1)
+    detection_result = motion_detector.detect(frame_1)
     """検知結果、検知した動体の位置"""
-    print("frame_1 status = {}".format(status))
-    if status is True:
+    print("frame_1 status = {}".format(detection_result.has_detect))
+    if detection_result.has_detect is True:
+        rect = detection_result.detected_area
         # 検知した動体の位置に矩形を描画
-        print("detected_area = {}".format(detected_area))
+        print("detected_area = {}".format(rect))
         # 現フレームをコピー
         img = frame_1.copy()
         RECT_COLOR = (0, 255, 0)
@@ -64,10 +71,13 @@ def test_motion_detection():
         """矩形の線の太さ"""
         cv2.rectangle(
             img,
-            (detected_area[0], detected_area[1]),
             (
-                detected_area[0] + detected_area[2],
-                detected_area[1] + detected_area[3],
+                rect[0],
+                rect[1],
+            ),
+            (
+                rect[0] + rect[2],
+                rect[1] + rect[3],
             ),
             RECT_COLOR,
             RECT_THICKNESS,
